e63c5a0c678f8fbbdb2545e00d28b7395223cafe
floyd <floyd.zweydinger@rub.de>
2025-04-03 22:17:44 +0200

    updated some things

--- python_nano_bench/nano_bench.py
+++ python_nano_bench/nano_bench.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 """ wrapper around the `./nanoBench` command """
-import atexit
-import os
 import re
 import subprocess
 import sys
@@ -9,7 +7,6 @@ from typing import Union, List, Tuple
 from subprocess import Popen, PIPE, STDOUT
 from pathlib import Path
 
-from collections import OrderedDict
 from shutil import copyfile
 
 PFC_START_ASM = '.quad 0xE0B513B1C2813F04'
@@ -21,6 +18,29 @@ class NanoBench:
     wrapper around ./nanoBench
     """
     def __init__(self):
+        self.kernel_mode = False
+
+        # nanoBennch kernel and user params
+        self._verbose = False
+        self._remove_empty_events = False
+        self._no_mem = False
+        self._range = False
+        self._min = False
+        self._max = False
+        self._median = False
+        self._avg = False
+        self._alignment_offset = False
+        self._initial_warm_up_count = False
+        self._warm_up_count = False
+        self._n_measurements = False
+        self._loop_count = False
+        self._unroll_count = False
+        self._cpu = False
+        self._code_one_time_init = False
+        self._code_late_init = False
+        self._code_init = False
+        self._end_to_end = False
+
         pass
 
     @staticmethod
@@ -51,26 +71,44 @@ class NanoBench:
         return True
 
     @staticmethod
-    def write_file(filename: str, content: Union[str, bytes], root: bool):
-        """ simple wrapper to write to a file"""
+    def write_file(filename: Union[str, Path],
+                   content: Union[str, bytes],
+                   root: bool=False) -> bool:
+        """ simple wrapper to write to a file
+        :param filename: full path to the file to write to
+        :param content: the content to write to the file
+        :param root: if true, temporary root rights are needed to write to the 
+            given file. Hence a special code path is used.
+        """
         if root:
             pass  # TODO
+            return False
         else:
-            with open(filename, 'w') as f:
+            with open(filename, 'w', encoding="utf-8") as f:
                 f.write(str(content))
+                return True
 
     @staticmethod
-    def read_file(filename: str, root: bool):
-        """ simple wrapper to read form a file"""
+    def read_file(filename: Union[str, Path],
+                  root: bool=False) -> str:
+        """ simple wrapper to read form a file
+        :param filename: the full path to read from
+        :param root: if true, temporary root rights are needed to write to the 
+            given file. Hence a special code path is used.
+        :return the content of the file as a string
+        """
         if root:
             pass  # TODO
+            return ""
         else:
             with open(filename) as f:
                 return f.read()
 
     @staticmethod
-    def run_command(cmds: List[str], root: bool) -> Tuple[bool, str]:
+    def run_command(cmds: List[str],
+                    root: bool) -> Tuple[bool, str]:
         """
+        :param cmfd
         """
         if root:
             # TODO different super user command
@@ -87,7 +125,9 @@ class NanoBench:
         return True, s
 
     @staticmethod
-    def assemble(code: str, obj_file: str, asm_file: str = '/tmp/ramdisk/asm.s'):
+    def assemble(code: str, 
+                 obj_file: str,
+                 asm_file: str = '/tmp/ramdisk/asm.s'):
         """
         needs `as`
 
@@ -126,7 +166,8 @@ class NanoBench:
             return False
 
     @staticmethod
-    def objcopy(source_file: str, target_file: str):
+    def objcopy(source_file: str,
+                target_file: str):
         """
         copy the code/text section from `source_file` to `target_file`
 
@@ -166,35 +207,38 @@ class NanoBench:
         return False
 
     @staticmethod
-    def getR14Size(self) -> int:
+    def getR14Size() -> int:
         """
-        Returns the size in bytes.
+        NOTE: only available if the kernel module is loaded.
+        :return the size in bytes.
         """
-        if not hasattr(self.getR14Size, 'r14Size'):
+        if not hasattr(NanoBench.getR14Size, 'r14Size'):
             with open('/sys/nb/r14_size') as f:
                 line = f.readline()
                 mb = int(line.split()[2])
-                self.getR14Size.r14Size = mb * 1024 * 1024
-        return self.getR14Size.r14Size
+                NanoBench.getR14Size.r14Size = mb * 1024 * 1024
+        return NanoBench.getR14Size.r14Size
 
     @staticmethod
     def getAddress(reg):
-        """
-        Returns the address that is stored in R14, RDI, RSI, RBP, or RSP as a hex string.
+        """ Returns the address that is stored in R14, RDI, RSI, RBP, or RSP 
+        as a hex string.
+        NOTE: only available if the kernel module is loaded
+        :param reg: register name
         """
         with open('/sys/nb/addresses') as f:
             for line in f:
                 lReg, addr = line.strip().split(': ')
                 if reg.upper() == lReg:
                     return addr
-        raise ValueError('Address not found')
+        raise ValueError('Register/Address not found')
 
     @staticmethod
-    def is_HT_enabled(cls) -> bool:
+    def is_HT_enabled() -> bool:
         """ checks whether hyper threading is enabled
         : returns true/False if HT is enabled or not
         """
-        t = cls.read_file("/sys/devices/system/cpu/smt/activeA")
+        t = NanoBench.read_file("/sys/devices/system/cpu/smt/active", False)
         try:
             t = int(t)
             t = t != 0
@@ -205,9 +249,7 @@ class NanoBench:
 
     @staticmethod
     def set_HT(state: int) -> bool:
-        """
-        NOTE: Needs root rights
-
+        """ NOTE: Needs root rights
         :param state: either 0 for disable HT
                           or 1 to enable HT
 
@@ -262,14 +304,98 @@ class NanoBench:
         return True
 
     def run(self, asm: str) -> bool:
+        """
+        """
         if not self.prefix():
             return False
 
         obj_file = "TODO"
-        NanoBench.run_command(["deps/nanoBench/user/nanoBench", obj_file], True)
+        cmd = ["deps/nanoBench/user/nanoBench", obj_file]
+        if self._verbose: cmd += "--verbose"
+        if self._remove_empty_events: cmd += "--remove_empty_events"
+        NanoBench.run_command(cmd, True)
 
         if not self.postfix():
             return False
 
         return True
+
+    def verbose(self) -> 'NanoBench':
+        """Outputs the results of all performance counter readings."""
+        self._verbose = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def no_mem(self) -> 'NanoBench':
+        """"""
+        self._no_mem = True
+        return self
+
+    def range(self) -> 'NanoBench':
+        """"""
+        self._ran = True
+        return self
+
+    def remove_empgety_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
+    def remove_empty_events(self) -> 'NanoBench':
+        """"""
+        self._remove_empty_events = True
+        return self
 
